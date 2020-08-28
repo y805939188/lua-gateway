49d682ca95f0d1bf0d1a5d37b7496293acb9f49e
dingyubo <805939188@qq.com>
2020-08-28 14:21:36 +0800

    update

--- lua/balaner.lua
+++ lua/balaner.lua
@@ -6,11 +6,5 @@ tmpUpstreams = cjson.decode(tmpUpstreams);
 local pathAndPort = tmpUpstreams[math.random(1, table.getn(tmpUpstreams))];
 local currentPath = pathAndPort.path
 local currentPort = pathAndPort.port
--- if (pathAndPort.type == 'domain') then
---   currentPort = pathAndPort.port
--- end
-
--- pathAndPort.port = pathAndPort.port and pathAndPort.port or 80
-ngx.log(ngx.INFO, "这里的port是:: "..currentPort)
 ngx.log(ngx.INFO, "test current path: port ===========> "..currentPath..":"..currentPort)
 balancer.set_current_peer(currentPath, currentPort and currentPort or 80);
